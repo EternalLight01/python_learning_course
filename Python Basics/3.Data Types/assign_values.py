05169a28a54189743e2fe67e1f27469a2d2ad57f
EternalLight01 <kjhlkjhlkjhl2011@gmail.com>
2023-11-05 20:23:41 +0200

    declared variables for different types

---
+++ Python Basics/3.Data Types/assign_values.py
@@ -0,0 +1,13 @@
+# Declare the following variables:
+# brand with the meaning "Toyota"
+# price with a value of 22500
+# is_sedan with value True
+# owner with a value of None.
+# For example, if you wanted to declare a name variable with the value "Ivan", it would look like this:
+# name = "Ivan"
+
+brand = "Toyota"
+price = 22500
+is_sedan = "True"
+owner = None
+
