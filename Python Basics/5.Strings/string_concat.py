ea4d20464c0118ba0a6a71da7295c32afdbf1888
EternalLight01 <kjhlkjhlkjhl2011@gmail.com>
2023-11-05 20:41:15 +0200

    completed the task on the topic of string

---
+++ Python Basics/5.Strings/string_concat.py
@@ -0,0 +1,15 @@
+# Declare a new variable result_string. Its value is "glued together" from the already declared variables a, b and c, so that the result is a "Concatenation" string.
+
+# Example:
+# a = "llo"
+# b = "He"
+# result = b + a + "!"  # Contains the string "Hello!
+
+a = "Con"
+b = "enation"
+c = "cat"
+# write your code below
+
+result_string = a + c + b
+
+print(result_string)
---
+++ Python Basics/5.Strings/string_interpolation.py
@@ -0,0 +1,13 @@
+# Declare a new variable result_string. To write its value, use the already declared variables a and b. 
+# The result should be the following line: Hello, world!.
+# Pay attention to the space, comma and exclamation point that should be in the result.
+# Example:
+# a = "Hello"
+# result = f"{a}!!!"  # contains the string "Hello!!!"
+
+a = "Hello"
+b = "world"
+# write your code below this line
+
+result_string = f"{a}, {b}!"
+
